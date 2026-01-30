fc64300a30ba51f3231f97b5350c11d0591cd1c0
Florian Imdahl <git@ffflorian.de>
2026-01-30 11:51:43 +0100

    fix(eslint-config): Add browser globals

--- packages/eslint-config/index.ts
+++ packages/eslint-config/index.ts
@@ -19,6 +19,7 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: {
       globals: {
+        ...globals.browser,
         ...globals.node,
       },
     },
